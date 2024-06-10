e4e1393cd9fb54451c57b9a6d85b249c014a2f6d
gxldmane <timofey.hodotaev@yandex.ru>
2024-06-10 18:45:04 +0500

    RankTest.php fixes

--- backend/tests/Feature/RankTest.php
+++ backend/tests/Feature/RankTest.php
@@ -1,7 +1,18 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use App\Models\Rank;
+
+it ('get rank by id', function () {
+    Rank::factory(1)->create();
+
+    $response = $this->get('/api/v1/rank/1');
 
     $response->assertStatus(200);
+});
+
+
+it('failed to get rank by id', function () {
+    $response = $this->get('/api/v1/rank/999');
+
+    $response->assertStatus(404);
 });

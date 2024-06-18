0dadffb02fcb270ff26aefda39ce8f366da993e4
gxldmane <timofey.hodotaev@yandex.ru>
2024-06-18 16:52:47 +0500

    add name column to user_media table

--- backend/database/migrations/2024_06_04_144411_create_user_media_table.php
+++ backend/database/migrations/2024_06_04_144411_create_user_media_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('user_media', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->foreignId('user_id')->constrained();
             $table->string('media_path');
             $table->timestamps();
